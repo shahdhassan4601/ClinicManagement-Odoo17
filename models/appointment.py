25d4330eac9119ce09d51baf0376e2c097b1562c
Shahd Hassan <shahd.hassan.osh@gmail.com>
2024-06-30 22:58:45 +0300

    -  create new appointment model and add some basic fields

---
+++ models/appointment.py
@@ -0,0 +1,27 @@
+from odoo import api, fields, models
+
+
+class Appointment(models.Model):
+    _name = 'clinic.appointment'
+    _description = 'Appointment'
+
+    # Base fields
+    name = fields.Char('Name', required=True)
+    patient_id = fields.Many2one('res.partner', string='Patient', required=True)
+    datetime = fields.Datetime('Date and Time', required=True)
+    doctor_id = fields.Many2one('res.users', string='Doctor', required=True)
+    appointment_type = fields.Selection([
+        ('consultation', 'Consultation'),
+        ('emergency', 'Emergency'),
+        ('checkup', 'Checkup'),
+        ('surgery', 'Surgery'),
+    ], string='Appointment Type', required=True)
+    
+    status = fields.Selection([
+        ('pending', 'Pending'),
+        ('cancelled', 'Cancelled'),
+        ('confirmed', 'Confirmed'),
+    ], default='pending', required=True)
+    
+    notes = fields.Text('Notes')
+    
