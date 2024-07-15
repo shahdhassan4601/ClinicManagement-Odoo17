8071b3d0ad2bef1a92a33bee89c18d64c7241e1e
Shahd Hassan <shahd.hassan.osh@gmail.com>
2024-07-15 12:13:45 +0300

    add sequence to the prescription

--- models/clinic_prescription.py
+++ models/clinic_prescription.py
@@ -5,9 +5,11 @@ class ClinicPrescription(models.Model):
     _name = 'clinic.prescription'
     _description = 'Prescription'
 
+    name = fields.Char(string='Name', readonly=True, default='New')
     doctor_id = fields.Many2one('res.users', string='Doctor')
     patient_id = fields.Many2one('res.partner', string='Patient')
     treatment_id = fields.Many2one('clinic.treatment', string='Treatment')
+    appointment_id = fields.Many2one('clinic.appointment', string='Appointment')
     
     # medication details
     medication_name = fields.Char(string='Medication Name')
@@ -15,3 +17,8 @@ class ClinicPrescription(models.Model):
     frequency = fields.Integer(string='Frequency (hours)')
     
     notes = fields.Text(string='Notes')
+    
+    @api.model
+    def create(self, vals):
+        vals["name"] = self.env['ir.sequence'].next_by_code('prescription.sequence')
+        return super().create(vals)
