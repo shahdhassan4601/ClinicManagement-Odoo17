351ed7c91317aae524a717c4e039d9bc747f1b5b
Shahd Hassan <shahd.hassan.osh@gmail.com>
2024-07-15 11:03:00 +0300

    doctor availability: - edit the create method to create appointments once the doctor assign their availability time. - handle the time zone also

--- models/clinic_doctor_availability.py
+++ models/clinic_doctor_availability.py
@@ -1,4 +1,4 @@
-from datetime import date, datetime, time, timedelta
+from datetime import date, datetime, time, timedelta, timezone
 from xml.dom import ValidationErr
 import pytz
 
@@ -19,44 +19,42 @@ class DoctorAvailability(models.Model):
         ('2', 'Wednesday'),
         ('3', 'Thursday')
     ])
-    start_datetime = fields.Float(string='Start Time')
-    end_datetime = fields.Float(string='End Time')
+    start_datetime = fields.Float(string='Start Time', widget="float_time")
+    end_datetime = fields.Float(string='End Time', widget="float_time")
     
     @api.model
     def create(self, vals):
 
         res = super(DoctorAvailability, self).create(vals)
         
-        hours = int(res.start_datetime)  # Get the integer part for hours
-        minutes = int((res.start_datetime - hours) * 60)  # Calculate minutes
-
+        current_date = datetime.now()
+        current_date = current_date.astimezone(pytz.timezone('UTC')).replace(tzinfo=None)
         
-        current_date = datetime.combine(date.today(), time(hours, minutes, 0, tzinfo=pytz.UTC))
-        current_date = current_date.astimezone(pytz.timezone('UTC'))
+          
+        end_date = current_date + timedelta(weeks=4)
         
-        # raise models.ValidationError(f'{current_date}')
-        
-        # current_date = current_date + timedelta(hours=3)   
-        end_date = current_date + timedelta(days=30)
-        
-        diff = 1
         while current_date <= end_date:
             
             if current_date.weekday() == int(vals["week_day"]):
                 
-                x = (vals['end_datetime'] - vals['start_datetime']) * 3 
-                while x:
+                end_time = vals['end_datetime'] 
+                start_time = vals['start_datetime']
+                
+                while start_time <= end_time:
+                    
+                    appointment_date = datetime.combine(current_date, datetime.min.time()) + timedelta(hours=start_time)
+                    
+                    cairo_tz = pytz.timezone('Africa/Cairo')
+                    appointment_date = cairo_tz.localize(appointment_date, is_dst=None)
+                    appointment_date = appointment_date.astimezone(pytz.timezone('UTC')).replace(tzinfo=None)
+                    
+                    
                     self.env['clinic.appointment'].create({
                         'doctor_id': res.doctor_id.id,
-                        'datetime': current_date.strftime("%Y-%m-%d %H:%M:%S"),
+                        'datetime': appointment_date
                     })
-                    current_date += timedelta(hours=1/3)
-                    x-=1
-                # reset curent date time to 
-            current_date = datetime.combine(date.today() + timedelta(days=diff), time(hours, minutes, 0), tzinfo=pytz.UTC)
-            current_date = current_date.astimezone(pytz.timezone('UTC'))
-            # current_date = current_date + timedelta(hours=3)   
-            
-            diff += 1
-    
+                    start_time += 0.25
+                    
+            current_date += timedelta(days=1)
+                
         return res
