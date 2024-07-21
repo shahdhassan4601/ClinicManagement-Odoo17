d31fd9de40038ad4c5c83a2216d454d52730985f
Shahd Hassan <shahd.hassan.osh@gmail.com>
2024-07-21 09:50:17 +0300

    - edit some fields in medical record - edit in the form view of medical record

--- models/clinic_medical_record.py
+++ models/clinic_medical_record.py
@@ -5,11 +5,13 @@ class ClinicMedicalRecord(models.Model):
     _name = 'clinic.medical.record'
     _description = 'Medical Record'
 
-    date = fields.Date(string='Date')
     patient_id = fields.Many2one('res.partner', string='Patient')
     doctor_id = fields.Many2one('res.users', string='Doctor')
     appointment_id = fields.Many2one('clinic.appointment', string='Appointment')
-    treatment_ref = fields.Many2many('clinic.treatment', string='Treatment')
+    appointment_datetime = fields.Datetime(string='Appointment Date', related='appointment_id.datetime', store=True)
+    treatment_ref = fields.One2many('clinic.treatment', 'medical_record_id',string='Treatment')
+    prescription_id= fields.One2many('clinic.prescription','medical_record_id', string='Prescription')
+    
     is_uploadable_files = fields.Boolean(string='Uploadable Files')
     attachment_ids = fields.Many2many('ir.attachment', string='Attachments')
     notes = fields.Text(string='Notes')
